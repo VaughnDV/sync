1fcdbbd611f848ee81642d12e3575fd3eaddb186
VaughnDV <vaughndevilliers@gmail.com>
2025-04-23 14:56:06 +0100

    Project setup

--- src/sync/settings.py
+++ src/sync/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'sync'),
         'USER': os.getenv('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
         'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
