7997251a88aa83af3f44059eef5e3cc85afdb536
VaughnDV <vaughndevilliers@gmail.com>
2025-04-24 10:46:57 +0100

    Project setup

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class User(AbstractUser):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
-    spotify_access_token = models.CharField(max_length=255, blank=True, null=True)
-    spotify_refresh_token = models.CharField(max_length=255, blank=True, null=True)
+    spotify_access_token = models.CharField(max_length=1000, blank=True, null=True)
+    spotify_refresh_token = models.CharField(max_length=1000, blank=True, null=True)
     spotify_token_expires_at = models.DateTimeField(null=True, blank=True)
 
     USERNAME_FIELD = 'email'
