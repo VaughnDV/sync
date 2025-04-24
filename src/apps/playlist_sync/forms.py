d1f90a90ff52844de12cae45ada5bb461af2c28f
VaughnDV <vaughndevilliers@gmail.com>
2025-04-24 10:56:07 +0100

    Project setup

--- src/apps/playlist_sync/forms.py
+++ src/apps/playlist_sync/forms.py
@@ -6,9 +6,17 @@ class SyncJobForm(forms.ModelForm):
         model = SyncJob
         fields = ['youtube_playlist_url', 'spotify_playlist_name', 'reverse_order']
         widgets = {
-            'youtube_playlist_url': forms.URLInput(attrs={'class': 'form-control', 'placeholder': 'Enter YouTube playlist URL'}),
-            'spotify_playlist_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Spotify playlist name'}),
-            'reverse_order': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'youtube_playlist_url': forms.URLInput(attrs={
+                'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500',
+                'placeholder': 'Enter YouTube playlist URL'
+            }),
+            'spotify_playlist_name': forms.TextInput(attrs={
+                'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500',
+                'placeholder': 'Enter Spotify playlist name'
+            }),
+            'reverse_order': forms.CheckboxInput(attrs={
+                'class': 'h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded'
+            }),
         }
 
 class TrackMappingForm(forms.Form):
