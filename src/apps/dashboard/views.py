c7a9f596ea400fd09b45ef231234c85eba58392f
VaughnDV <vaughndevilliers@gmail.com>
2025-04-25 12:40:40 +0100

    Testing integrations

--- src/apps/dashboard/views.py
+++ src/apps/dashboard/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from apps.playlist_sync.models import SyncJob
 
 @login_required
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html') 
+    recent_syncs = SyncJob.objects.filter(user=request.user).order_by('-created_at')[:10]
+    context = {
+        'recent_syncs': recent_syncs
+    }
+    return render(request, 'dashboard/dashboard.html', context) 
