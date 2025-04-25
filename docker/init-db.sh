5bdbcac690fc79cd7e7594426967d865da590e04
VaughnDV <vaughndevilliers@gmail.com>
2025-04-25 17:58:03 +0100

    Testing integrations

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 set -e
 
-# Create the postgres user if it doesn't exist
+# Create the postgres user and database
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    DO \$\$
-    BEGIN
-        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'postgres') THEN
-            CREATE USER postgres WITH SUPERUSER PASSWORD 'postgres';
-        END IF;
-    END
-    \$\$;
+    CREATE USER postgres WITH SUPERUSER PASSWORD 'postgres';
     GRANT ALL PRIVILEGES ON DATABASE postgres TO postgres;
     ALTER USER postgres WITH LOGIN;
 EOSQL 
