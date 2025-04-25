1208ad0d1ccb731f3c5cf27c571571f4d8f2793a
VaughnDV <vaughndevilliers@gmail.com>
2025-04-25 12:53:26 +0100

    Testing integrations

--- src/apps/playlist_sync/forms.py
+++ src/apps/playlist_sync/forms.py
@@ -4,7 +4,7 @@ from .models import SyncJob
 class SyncJobForm(forms.ModelForm):
     class Meta:
         model = SyncJob
-        fields = ['youtube_playlist_url', 'spotify_playlist_name', 'reverse_order']
+        fields = ['youtube_playlist_url', 'spotify_playlist_name', 'spotify_playlist_id', 'reverse_order']
         widgets = {
             'youtube_playlist_url': forms.URLInput(attrs={
                 'class': 'mt-1 block w-full rounded-md bg-gray-700 border-gray-600 text-gray-200 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 placeholder-gray-400',
@@ -14,6 +14,7 @@ class SyncJobForm(forms.ModelForm):
                 'class': 'mt-1 block w-full rounded-md bg-gray-700 border-gray-600 text-gray-200 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 placeholder-gray-400',
                 'placeholder': 'Enter Spotify playlist name'
             }),
+            'spotify_playlist_id': forms.HiddenInput(),
             'reverse_order': forms.CheckboxInput(attrs={
                 'class': 'h-4 w-4 bg-gray-700 border-gray-600 text-indigo-600 focus:ring-indigo-500 focus:ring-offset-gray-800 rounded'
             }),
