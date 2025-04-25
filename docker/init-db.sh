a83c697bc43240a6804e7525d735efca9fe5790f
VaughnDV <vaughndevilliers@gmail.com>
2025-04-25 18:55:56 +0100

    Testing integrations

--- docker/init-db.sh
+++ docker/init-db.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e
 
-# Create the rdb user and database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+# Create the postgres user and database
+psql -v ON_ERROR_STOP=1 --username "postgres" <<-EOSQL
     DO \$\$
     BEGIN
-        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'rdb') THEN
-            CREATE USER rdb WITH SUPERUSER PASSWORD 'postgres';
+        IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'postgres') THEN
+            CREATE USER postgres WITH SUPERUSER PASSWORD 'postgres';
         END IF;
     END
     \$\$;
-    GRANT ALL PRIVILEGES ON DATABASE vaughndv TO rdb;
-    ALTER USER rdb WITH LOGIN;
+    CREATE DATABASE vaughndv;
+    GRANT ALL PRIVILEGES ON DATABASE vaughndv TO postgres;
+    ALTER USER postgres WITH LOGIN;
 EOSQL 
